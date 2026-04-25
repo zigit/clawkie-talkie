13da82a9a860a9e6e8fa3607ea5a18e551c3d648
David Guttman <david@davidguttman.com>
2026-04-25 08:33:46 -0700

    Define browser global for simple-peer

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
   server: {
     host: true,
     allowedHosts: true,
